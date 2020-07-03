49a374babce58271fb97720fb257dc6f32c4da6a
MJmajun <45744094+MJmajun@users.noreply.github.com>
2020-07-03 20:25:48 +0800

    socket环境
    初始socket环境已经搭配好

--- EeayTcpClient/EeayTcpClient/client.cpp
+++ EeayTcpClient/EeayTcpClient/client.cpp
@@ -1,6 +1,15 @@
-#include "stdio.h"
-
+#define WIN32_LEAN_AND_MEAN
+#include <WinSock2.h>
+#include <windows.h>
+//#pragma comment(lib,"ws2_32.lib");	//解决库调用  我们用通用的方法 已经在属性中添加了
 int main()
 {
+	//启动window socket 环境
+	WORD ver = MAKEWORD(2,2);
+	WSADATA dat;
+	WSAStartup(ver,&dat);
+
+
+	WSACleanup();
 	return 0;
 }
