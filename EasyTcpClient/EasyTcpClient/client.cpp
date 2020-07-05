655555ae7f8bee14362f80936de2dc0bf8923525
MJmajun <1960844919@qq.com>
2020-07-05 15:59:29 +0800

    将client的代码改写了，可以子windows、和linux中都可以进行编译
    实现了跨平台

--- EasyTcpClient/EasyTcpClient/client.cpp
+++ EasyTcpClient/EasyTcpClient/client.cpp
@@ -1,12 +1,24 @@
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#ifdef _WIN32
 #include <WinSock2.h>
 #include <windows.h>
+#else
+#include <unistd.h>             //unix的标准库
+#include <arpa/inet.h>
+#include <string.h>
+
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define SOCKET_ERROR (-1)
+#endif
+
 #include <stdio.h>
 #include <thread>
 
-//#pragma comment(lib,"ws2_32.lib");	//解决库调用  我们用通用的方法 已经在属性中添加了
-enum CMD	//枚举登录和登出
+//#pragma comment(lib,"ws2_32.lib");    //解决库调用  我们用通用的方法 已经在属性中添加了
+enum CMD        //枚举登录和登出
 {
 	CMD_LOGIN,
 	CMD_LOGIN_RESULT,
@@ -16,13 +28,13 @@ enum CMD	//枚举登录和登出
 	CMD_ERROR
 };
 
-struct DataHeader	//定义数据包头
+struct DataHeader       //定义数据包头
 {
 	short dataLength;
 	short cmd;
 };
 
-struct Login : public DataHeader		//定义一个结构体封装数据
+struct Login : public DataHeader                //定义一个结构体封装数据
 {
 	Login()
 	{
@@ -33,7 +45,7 @@ struct Login : public DataHeader		//定义一个结构体封装数据
 	char passWord[32];
 };
 
-struct LoginResult : public DataHeader	//返回登录的结果
+struct LoginResult : public DataHeader  //返回登录的结果
 {
 	LoginResult()
 	{
@@ -44,7 +56,7 @@ struct LoginResult : public DataHeader	//返回登录的结果
 	int result;
 };
 
-struct Logout : public DataHeader		//返回谁要退出
+struct Logout : public DataHeader               //返回谁要退出
 {
 	Logout()
 	{
@@ -53,7 +65,7 @@ struct Logout : public DataHeader		//返回谁要退出
 	}
 	char username[32];
 };
-struct LogoutResult : public DataHeader		//返回登出的结果
+struct LogoutResult : public DataHeader         //返回登出的结果
 {
 	LogoutResult()
 	{
@@ -63,7 +75,7 @@ struct LogoutResult : public DataHeader		//返回登出的结果
 	}
 	int result;
 };
-struct NewUserJoin : public DataHeader		//新用户加入
+struct NewUserJoin : public DataHeader          //新用户加入
 {
 	NewUserJoin()
 	{
@@ -74,7 +86,7 @@ struct NewUserJoin : public DataHeader		//新用户加入
 	int scok;
 };
 
-int processor(SOCKET _cSock)	//专门处理接受到的消息
+int processor(SOCKET _cSock)    //专门处理接受到的消息
 {
 
 	//缓冲区
@@ -149,11 +161,12 @@ void cmdThread(SOCKET _sock)
 
 int main()
 {
+#ifdef _WIN32
 	//启动window socket 环境
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA dat;
 	WSAStartup(ver, &dat);
-
+#endif
 	//1、建立一个套接字
 	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (INVALID_SOCKET == _sock)
@@ -169,7 +182,11 @@ int main()
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
-	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+#ifdef _WIN32
+	_sin.sin_addr.S_un.S_addr = inet_addr("192.168.1.188");
+#else
+	_sin.sin_addr.s_addr = inet_addr("192.168.1.188");
+#endif
 	int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
 	if (SOCKET_ERROR == ret)
 	{
@@ -181,16 +198,16 @@ int main()
 	}
 
 	//启动线程
-	std::thread t1(cmdThread, _sock);	//第一个是函数名 第二个就是要传入的参数
-	t1.detach();		//和主线程进行分离  一定要进行分离，不然子线程的退出会直接导致主线程也退出，但是，主线程没有正常结束程序，就会产生问题
+	std::thread t1(cmdThread, _sock);       //第一个是函数名 第二个就是要传入的参数
+	t1.detach();            //和主线程进行分离  一定要进行分离，不然子线程的退出会直接导致主线程也退出，但是，主线程没有正常结束程序，就会产生问题
 	while (g_bRun)
 	{
 		fd_set fdRead;
 		FD_ZERO(&fdRead);
 		FD_SET(_sock, &fdRead);
 
-		timeval t = { 1,0 };//前面是秒 后面是毫秒	让select每隔1秒去扫描一下
-		int ret = select(_sock, &fdRead, 0, 0, &t);
+		timeval t = { 1,0 };//前面是秒 后面是毫秒       让select每隔1秒去扫描一下
+		int ret = select(_sock + 1, &fdRead, 0, 0, &t);
 
 		if (ret < 0)
 		{
@@ -206,9 +223,12 @@ int main()
 			}
 		}
 	}
-
+#ifdef _WIN32
 	WSACleanup();
 	closesocket(_sock);
+#else
+	close(_sock);
+#endif
 	getchar();
 	return 0;
 }
